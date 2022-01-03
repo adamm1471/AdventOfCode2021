f81060f6eefda5abe6b84da97ed5ce255b4422e7
adamm <adamm@VALHALLA>
2022-01-03 05:35:28 +0100

    Formatting.  Add project files.

---
+++ Day09/Program.cs
@@ -0,0 +1,151 @@
+﻿using System.Numerics;
+
+List<string>? data = null;
+using (var sr = new StreamReader(@"input.txt"))
+    data = sr.ReadToEnd().Split($"\r\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+int x = data[0].Length;
+int y = data.Count();
+
+int[,] heightmap = new int[x, y];
+
+for(int i = 0; i < y; i++) // iterate over row
+{
+    for (int j = 0; j < x; j++)
+    {
+        heightmap[j, i] = Convert.ToInt32(data[i][j].ToString());
+    }
+}
+
+PartOne(heightmap);
+PartTwo(heightmap);
+
+void PartOne(int[,] map)
+{
+    int xmax = map.GetLength(0);
+    int ymax = map.GetLength(1);
+
+    List<int> lowPoints = new();
+
+    for (int y = 0; y < ymax; y++)
+    {
+        for (int x = 0; x < xmax; x++)
+        {
+            int currentNumber = map[x, y];
+            List<(int, int)> proximity = new List<(int, int)> { (0, -1), (-1, 0), (1, 0), (0, 1) };
+            List<int> surrounding = new();
+            foreach (var (p1, p2) in proximity)
+            {
+                try
+                {
+                    surrounding.Add(map[x + p1, y + p2]);
+                }
+                catch (IndexOutOfRangeException) { }
+            }
+            if (currentNumber < surrounding.Min())
+                lowPoints.Add(currentNumber);
+        }
+    }
+
+    BigInteger total = lowPoints.Sum() + (lowPoints.Count);
+    Console.WriteLine($"Part One. The total is {total}.");
+}
+
+void PartTwo(int[,] map)
+{
+
+    int xmax = map.GetLength(0);
+    int ymax = map.GetLength(1);
+
+    List<(int p1, int p2)> lowPoints = new();
+    for (int y = 0; y < ymax; y++)
+    {
+        for (int x = 0; x < xmax; x++)
+        {
+            int currentNumber = map[x, y];          
+            List<(int, int)> proximity = new List<(int, int)> { (0, -1), (-1, 0), (1, 0), (0, 1) };
+            List<int> surrounding = new();
+            foreach (var (p1, p2) in proximity)
+            {
+                try
+                {
+                    surrounding.Add(map[x + p1, y + p2]);
+                }
+                catch (IndexOutOfRangeException)
+                { }
+            }
+            if (currentNumber < surrounding.Min())
+            {
+                lowPoints.Add( (x, y) );
+            }
+        }
+    }
+
+    List<(int size, (int px, int py))> lowPointSizes = new();
+    foreach ( (int lpx, int lpy) lp in lowPoints)
+    {
+        HashSet<(int, int)> done = new();
+        List<(int, int)> stack = new(); 
+
+        foreach ( (int, int) neighbor in GetValidNeighbors(map, lp))
+        {
+            if( !done.Contains(neighbor) )
+                stack.Add(neighbor); 
+        }
+        while (stack.Count > 0)
+        {
+            (int, int) p = stack.Last();
+            stack.Remove(p);
+
+            foreach ( (int,int) neighbor in GetValidNeighbors(map, p))
+            {
+                if ( !done.Contains( neighbor ))
+                    stack.Add( neighbor );
+            }
+            done.Add(p);
+        }
+        lowPointSizes.Add( (done.Count, lp) );
+    }
+    lowPointSizes.Sort((s, t) => t.size.CompareTo(s.size));
+    int productOfThreeLargest = lowPointSizes[0].size * lowPointSizes[1].size * lowPointSizes[2].size;
+    Console.WriteLine($"Part Two.  The product of the three largest basins is {productOfThreeLargest}.");
+
+}
+
+List<(int, int)> GetValidNeighbors(int[,] map, (int x, int y) sp )
+{
+    List<(int, int)> proximity = new List<(int, int)> { (0, -1), (-1, 0), (1, 0), (0, 1) };
+    List<(int, int)> gn = new();
+    foreach (var (p1, p2) in proximity)
+    {
+        try
+        {
+            if (map[sp.x + p1, sp.y + p2] < 9)
+                gn.Add((sp.x + p1, sp.y + p2));
+        }
+        catch (IndexOutOfRangeException) { }
+    }
+    return gn;
+}
+
+/*
+static void PrintMap(int[,] map)
+{
+    for (int y = 0; y < map.GetLength(1); y++)
+    {
+        for (int x = 0; x < map.GetLength(0); x++)
+        {
+            if (map[x, y] == 0)
+            {
+                Console.Write("  ");
+            }
+            else
+            {
+                Console.Write($"{map[x, y]} ");
+            }
+            
+        }
+        Console.WriteLine();
+    }
+}
+*/
---
+++ Day18/Program.cs
@@ -0,0 +1,40 @@
+﻿// this is the data we need to represent as a binary tree
+string data = "[[[[[9,8],1],2],3],4]";
+
+
+
+
+// basic implementation
+// one issue; i want to represent a pair of numbers in a node.. maybe int[] _data,
+// then that could be the pair.  or the node could contain a single number.
+public class Node
+{
+    private Node Left;
+    private Node Right;
+    private int Value;
+
+    public Node(Node lnode, Node rnode)
+    {
+        Left = lnode;
+        Right = rnode;
+    }
+
+    public bool IsPairNode()
+    {
+        if (_data != null && _data.Count == 2)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // not sure about this one
+    public bool IsDataNode()
+    {
+        if (Left == null && Right == null)
+        {
+            return true;
+        }
+        return false;
+    }
+}
